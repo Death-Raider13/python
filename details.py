9cf97c76262bb9cefc3843a4ce6d79f934afb36a
Death-Raider13 <lateefedidi4@gmail.com>
2025-12-01 16:25:42 +0100

    Add files via upload

---
+++ details.py
@@ -0,0 +1,32 @@
+name = "Lateef"
+age = "17"
+city = "Lagos"
+weight = "70kg"
+height = "5.9ft"
+country = "Nigeria"
+Hobbies = "Reading, Playing Football, Coding"
+
+
+print ("Name:", name)
+print ("Data Type of Name is", type(name))
+print ("Age:", age)
+print ( "Data Type of Age is", type(age))
+print( "City:", city)
+print ("Data Type of City is", type(city))
+print ("Weight:", weight)
+print ("Data Type of Weight is", type(weight))
+print ("Height:", height)
+print ("Data Type of Height is", type(height))
+print ("Country:", country)
+print ("Data Type of Country is", type(country))
+print ("Hobbies:", Hobbies)
+print ("My Hobbies are", type(Hobbies))
+
+print("n/ After Type casting......")
+age = str(age)
+print(age)
+print("Data Type of Age is", type(age))
+weight = int(weight)
+print(weight)
+print("Data Type of Weight is", type(weight))
+
