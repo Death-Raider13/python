0bcfcca3973c9313468ffdf2b6987508ea11253c
Death-Raider13 <lateefedidi4@gmail.com>
2025-12-29 16:20:02 +0100

    Add files via upload

---
+++ tuple.py
@@ -0,0 +1,12 @@
+my_tuple =()
+print(my_tuple )    
+
+
+my_tuple = (1,2,3,4,5)
+print(my_tuple )
+
+my_tuple=(1, "Hello",3.4, True)
+print(my_tuple)
+
+my_tuple=("mouse", [1,2,3], (5,6,7))
+print(my_tuple)
