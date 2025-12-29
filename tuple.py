dd81c998b7253deb247a30e4dfcdc36c1164030e
Death-Raider13 <lateefedidi4@gmail.com>
2025-12-29 16:31:38 +0100

    s

--- tuple.py
+++ tuple.py
@@ -9,4 +9,15 @@ my_tuple=(1, "Hello",3.4, True)
 print(my_tuple)
 
 my_tuple=("mouse", [1,2,3], (5,6,7))
+
 print(my_tuple)
+
+
+my_tuple=("p","e","r","u","t","v")
+print(my_tuple[0])
+print(my_tuple[5])
+
+n_tuple =("mouse", [1,2,3], (5,6,7))
+
+print(n_tuple[0][3])
+print(n_tuple[1][1])
