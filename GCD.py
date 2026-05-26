69dbbc58557dbe626a4b1acb54c9e2c4f22e974e
Death-Raider13 <lateefedidi4@gmail.com>
2026-05-26 16:27:31 +0100

    Add files via upload

---
+++ GCD.py
@@ -0,0 +1,9 @@
+numberLargest = int(input("Enter Largest number:"))
+numberSmallest = int(input("Enter Smallest Number:"))
+
+while(numberSmallest):
+    numberStore = numberSmallest
+    numberSmallest = nuber Largest % numberSmallest
+    numberLargest = numberStore
+
+    print("HCF is:, numberLargest")
